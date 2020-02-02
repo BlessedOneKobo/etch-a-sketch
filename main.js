36cbcbcbe0669948983c777e1a164e3e0e6eb42d
BlessedOneKobo <BlessedOneKobo@protonmail.com>
2020-02-02 18:01:56 +0100

    Restructure implementation

--- main.js
+++ main.js
@@ -1,49 +1,90 @@
+const containerWidth = 580;
+const containerHeight = 580;
 const container = document.querySelector('.container');
+container.style.cssText = setStyleBox(containerWidth, containerHeight);
+container.style.backgroundColor = '#fff';
 
-const resetBtn = document.createElement('button');
-resetBtn.classList.add('reset');
-resetBtn.textContent = 'Reset';
+const buttons = document.querySelector('.buttons');
+const resetBtn = document.querySelector('button.reset');
+const rainbowBtn = document.querySelector('button.rainbow');
+
+buttons.style.width = `${containerWidth}px`;
 resetBtn.addEventListener('click', resetSquares);
-document.body.insertBefore(resetBtn, container);
-
+rainbowBtn.addEventListener('click', setRainbow);
 addSquares(16);
 
 // FUNCTIONS //
 
 function addSquares(numSquares) {
+	const width = containerWidth / numSquares;
+	const height = containerHeight / numSquares;
 	let squares = [];
+	
 	for (let i = 0; i < numSquares; i++) {
+		const row = document.createElement('div');
+		row.classList.add('row');
+		
 		for (let j = 0; j < numSquares; j++) {
-			const square = createSquare(numSquares);
-			container.appendChild(square);
+			const square = createSquare(numSquares)
+			row.appendChild(square);
 		}
+		
+		container.appendChild(row);
 	}
 }
 
 function createSquare(numSquares) {
 	const square = document.createElement('div');
+	const width = 1 / numSquares * containerWidth;
+	const height = 1 / numSquares * containerHeight;
 	
-	square.style.backgroundColor = '#000';
-	square.style.cursor = 'default';
 	square.classList.add('square');
+	square.style.cssText = setStyleBox(width, height);
+	square.style.backgroundColor = '#fff';
+	square.style.cursor = 'default';
 	square.addEventListener('mouseover', changeColor)
 	
 	return square;
 }
 
 function resetSquares() {
-	const squares = container.children;
-	[...squares].forEach(square => square.style.backgroundColor = '#000');
+	const numSquares = prompt('How many squares?');
+	
+	if (!isNaN(numSquares)) {
+		const squares = container.children;
+		[...squares].forEach(square => container.removeChild(square));
+		addSquares(numSquares);
+	}
+}
+
+function setRainbow(event) {
+	if (event.target.getAttribute('data-rainbow')) {
+		event.target.removeAttribute('data-rainbow');
+	} else {
+		event.target.setAttribute('data-rainbow', 'yes');
+	}
 }
 
 function changeColor(event) {
-	event.target.style.backgroundColor = '#fff';
+	if (rainbowBtn.getAttribute('data-rainbow')) {		
+		event.target.style.backgroundColor = randomColor();
+	} else {
+		event.target.style.backgroundColor = '#000';
+	}
 }
 
 function randomColor() {
-	return `rgba(${randomColor(255)},${randomColor(255)},${randomColor(255)})`;
+	const red = randomNumber(255).toString(16);
+	const green = randomNumber(255).toString(16);
+	const blue = randomNumber(255).toString(16);
+
+	return `#${red}${green}${blue}`;
+}
+
+function setStyleBox(width, height) {
+	return `width: ${width}px;height: ${height}px;flex: 1 1 ${width}px`;
 }
 
 function randomNumber(n) {
-	return Math.random() * (n + 1);
+	return Math.floor(Math.random() * (n + 1));
 }
